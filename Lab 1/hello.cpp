6692e1460ff7a6fd62589eb3440992f5703458b2
Justin Kuo <kuojusti@usc.edu>
2019-11-16 22:08:43 -0800

    Update hello.cpp

--- Lab 1/hello.cpp
+++ Lab 1/hello.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
         cout << "Provide your name on the command line" << endl;
         return 1;
     }
+    //prints out name from command line
     cout << "Hello World!" << endl;
     cout << "Welcome to CS103, " << argv[1] << endl;
     return 0;
