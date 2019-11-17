d0aa270a2630f92b5b0b6286435832467f3a24bc
Justin Kuo <kuojusti@usc.edu>
2019-11-16 22:14:12 -0800

    Add files via upload

---
+++ PA 1/abundant.cpp
@@ -0,0 +1,131 @@
+/********************************************************
+ * CS 103 PA 1: Abundant numbers
+ * Name: Justin Kuo
+ * USC email: kuojusti@usc.edu
+ * Comments (you want us to know):
+ *
+ * Description:
+ *   Displays the top 3 most "abundant" (see writeup)
+ *   numbers in the input sequence.  The input sequence
+ *   is assumed to be valid natural numbers separated
+ *   by spaces and ending with 0.
+ *
+ *   ABSOLUTELY NO ARRAY or `string`/`vector`/etc. usage
+ *   is allowed!
+
+ *   You may only declare: bool, int, or unsigned int
+ *   No library functions (beyond cin/cout) are allowed.
+ *
+ *   You may NOT modify any of the given template code.
+ *   This ensures our automated checkers can parse your
+ *   output.  Failure to follow this direction will lead
+ *   to a score of 0 on this part of the assignment.
+ ********************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // n1 is the number with the highest abundance, a1
+  // n2 is the number with the 2nd-highest abundance, a2
+  // n3 is the number with the 3rd-highest abundance, a3
+  unsigned int n1 = 0, n2 = 0, n3 = 0;
+  int a1 = 0, a2 = 0, a3 = 0;
+
+  // num_abundant counts how many numbers in the input
+  // sequence are abundant
+  int num_abundant = 0;
+
+  cout << "Enter a sequence of natural numbers, separated by spaces, ";
+  cout << "and ending with 0."<< endl;
+
+  /* Your code here */
+    int sum = 0;
+	int num;
+	int temp = 1;
+
+	while (cin >> num) {
+		if (num == 0) {
+			break;
+		}
+		for (int i = 1; i < num; i++) {
+			if (num % i == 0) {
+				sum += i;
+			}
+		}
+		if (sum > num) {
+			num_abundant++;
+			if (temp == 1) {
+				n1 = num;
+				a1 = sum;
+			}
+			if (temp == 2) {
+				if (sum > a1) {
+					n2 = n1;
+					a2 = a1;
+					n1 = num;
+					a1 = sum;
+				} else {
+					n2 = num;
+					a2 = sum;
+				}
+
+			}
+			if (temp == 3) {
+				if (sum > a2) {
+					if (sum > a1) {
+						n3 = n2;
+						a3 = a2;
+						n2 = n1;
+						a2 = a1;
+						n1 = num;
+						a1 = sum;
+					} else {
+						a3 = a2;
+						n3 = n2;
+						a2 = sum;
+						n2 = num;
+					}
+				} else {
+					a3 = sum;
+					n3 = num;
+				}
+			}
+			if (temp >= 4) {
+				if (sum > a2) {
+					if (sum > a1) {
+						n3 = n2;
+						a3 = a2;
+						n2 = n1;
+						a2 = a1;
+						n1 = num;
+						a1 = sum;
+					} else {
+						a3 = a2;
+						n3 = n2;
+						a2 = sum;
+						n2 = num;
+					}
+				} else {
+					a3 = sum;
+					n3 = num;
+				}
+			}
+		}
+		sum = 0;
+		temp++;
+	}
+    
+  /* End of your code */
+
+  cout << "Abundant number count: " << num_abundant << endl;
+  cout << "Top 3 most abundant numbers: " << endl;
+  cout << n1 << " : " << a1 << endl;
+  cout << n2 << " : " << a2 << endl;
+  cout << n3 << " : " << a3 << endl;
+
+
+  return 0;
+
+}
---
+++ PA 1/readme.txt
@@ -0,0 +1,53 @@
+CSCI 103 Abundant Commas Programming Assignment 
+
+Name: Justin Kuo
+
+Email Address: kuojusti@usc.edu
+
+NOTE: You can delete the questions, we only need your responses.
+
+NOTE: It is helpful if you write at most 80 characters per line,
+and avoid including special characters, so we can read your responses.
+
+Please summarize the help you found useful for this assignment, which
+may include office hours, discussion with peers, tutors, et cetera.
+Saying "a CP on Tuesday" is ok if you don't remember their name.
+If none, say "none".
+
+:
+
+================================ Questions ==================================
+
+1. I used a while loop because the number of inputs is unknown, so the best way to iterate through was using a for loop.
+   
+
+
+2. In order to find the proper divisors for a single input, I used a for loop because a for loop allows the divisor to increment each time.
+
+   
+3. 
+   The first idiom that I used was the divisibilty/factor idiom because in order to find the abundance, the divisors of a number need to be found,    then added together. This was used in my for loop, where I found each sum.
+
+   One idiom that I used was the swapping idiom, since that was required in order to be able to rank the top 3 abundances.
+
+   Another idiom that I used was the shifting values idiom. This is used when a number was found to be greater than the one already stored, so the    new sum had to overwrite a previous sum. This was used in my program each time when a later value was found to be larger than one that was in  t    he n1-n3 variables.
+    
+    
+
+
+================================ Remarks ====================================
+
+Filling in anything here is OPTIONAL.
+
+Approximately how long did you spend on this assignment?
+
+:
+
+Were there any specific problems you encountered? This is especially useful to
+know if you turned it in incomplete.
+
+:
+
+Do you have any other remarks?
+
+:
